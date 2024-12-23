6c42b95d992e225d209c6b407c0d633970a2cea4
Amogh Suman <amoghsuman@gmail.com>
2024-12-23 22:58:15 +0530

    Updates

---
+++ precache-manifest.f6e81b2c99bae54c41876c6d6efbcb17.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8f32daf4ba43fdf0f285c09e9ad1ea4",
+    "url": "/manpower-calculator/index.html"
+  },
+  {
+    "revision": "6676206eccd7aa748a53",
+    "url": "/manpower-calculator/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "638e200e69d950262938",
+    "url": "/manpower-calculator/static/js/2.ae026ab9.chunk.js"
+  },
+  {
+    "revision": "f457a91e29f5e6cff1f0",
+    "url": "/manpower-calculator/static/js/3.6757c1ca.chunk.js"
+  },
+  {
+    "revision": "6676206eccd7aa748a53",
+    "url": "/manpower-calculator/static/js/main.8db10ae2.chunk.js"
+  },
+  {
+    "revision": "bf96adf1c480da11e7a2",
+    "url": "/manpower-calculator/static/js/runtime~main.1f957475.js"
+  }
+]);
